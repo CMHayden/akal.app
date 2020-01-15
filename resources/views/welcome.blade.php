bea65d1b52c1c93eeef2012dd0ed96a2dd7394e7
Callum Hayden <cmhayden@Callums-MacBook-Pro.local>
2020-01-15 12:54:00 +0000

    Change laravel to akal on welcome page

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -81,7 +81,7 @@
 
             <div class="content">
                 <div class="title m-b-md">
-                    Laravel
+                    Akal
                 </div>
 
                 <div class="links">
