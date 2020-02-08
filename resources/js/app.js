2eb24305f082acccb011a97dad744076b8acf8ea
cmhayden <haydencallum4@gmail.com>
2020-02-08 19:10:14 +0000

    register user type form component with app js

--- resources/js/app.js
+++ resources/js/app.js
@@ -21,6 +21,11 @@ window.Vue = require('vue');
 
 Vue.component('example-component', require('./components/ExampleComponent.vue').default);
 
+Vue.component(
+    'form-user-type',
+    require('./components/formUserType.vue').default
+);
+
 /**
  * Next, we will create a fresh Vue application instance and attach it to
  * the page. Then, you may begin adding components to this application
