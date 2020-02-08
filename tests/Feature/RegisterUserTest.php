94d512d40cd4f579d2c349596fc98212d675f028
cmhayden <haydencallum4@gmail.com>
2020-02-08 19:03:02 +0000

    test user can view registration form

--- tests/Feature/RegisterUserTest.php
+++ tests/Feature/RegisterUserTest.php
@@ -5,18 +5,26 @@ namespace Tests\Feature;
 use Tests\TestCase;
 use Illuminate\Foundation\Testing\WithFaker;
 use Illuminate\Foundation\Testing\RefreshDatabase;
+use Illuminate\Support\Facades\Hash;
+use App\User;
 
 class RegisterUserTest extends TestCase
 {
+    use RefreshDatabase;
+
     /**
-     * A basic feature test example.
+     * Tests if a user can view the registration form
      *
-     * @return void
+     * @return true if status is 200
+     * @return true if view is auth.register
      */
-    public function testExample()
+    public function testUserCanViewRegistrationForm()
     {
-        $response = $this->get('/');
+        $response = $this->get('/register');
 
         $response->assertStatus(200);
+        $response->assertViewIs('auth.register');
     }
+
+
 }
