7a72cd1f41e7b07784d4b1430904377c0aaab5cc
cmhayden <haydencallum4@gmail.com>
2020-03-20 15:27:01 +0000

    create calendar model

--- app/Calendar.php
+++ app/Calendar.php
@@ -6,5 +6,10 @@ use Illuminate\Database\Eloquent\Model;
 
 class Calendar extends Model
 {
-    //
+    protected $fillable = [
+        'event_name',
+        'start_date',
+        'end_date',
+        'patientID'
+    ];
 }
