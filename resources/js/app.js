0b2683f570d46293dad5b8ff0d3c27d9072171d6
cmhayden <haydencallum4@gmail.com>
2020-03-20 15:30:07 +0000

    register the calendar component in app.js

--- resources/js/app.js
+++ resources/js/app.js
@@ -26,6 +26,11 @@ Vue.component(
     require('./components/formUserType.vue').default
 );
 
+Vue.component(
+    'calendar-component',
+    require('./components/CalendarComponent.vue').default
+);
+
 /**
  * Next, we will create a fresh Vue application instance and attach it to
  * the page. Then, you may begin adding components to this application
