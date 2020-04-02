c53eec90342a603ba694d734a7d0e1006c74cb17
cmhayden <haydencallum4@gmail.com>
2020-04-02 14:28:53 +0100

    update the calendar factory to match the calendar database fields

--- database/factories/CalendarFactory.php
+++ database/factories/CalendarFactory.php
@@ -7,6 +7,9 @@ use Faker\Generator as Faker;
 
 $factory->define(Calendar::class, function (Faker $faker) {
     return [
-        //
+        'event_name'    => $faker->name,
+        'start_date'    => '2020-04-01',
+        'end_date'      => '2020-04-01',
+        'patient_email' => $faker->safeEmail
     ];
 });
