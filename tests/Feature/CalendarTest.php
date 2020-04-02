a441c73f5b36d0c157113e8d14832defda6f5fc1
cmhayden <haydencallum4@gmail.com>
2020-04-02 14:29:44 +0100

    update the calendar test to test the full api

--- tests/Feature/CalendarTest.php
+++ tests/Feature/CalendarTest.php
@@ -5,6 +5,9 @@ namespace Tests\Feature;
 use Tests\TestCase;
 use Illuminate\Foundation\Testing\WithFaker;
 use Illuminate\Foundation\Testing\RefreshDatabase;
+use App\User;
+use App\Calendar;
+use Laravel\Passport\Passport;
 
 class CalendarTest extends TestCase
 {
@@ -13,10 +16,94 @@ class CalendarTest extends TestCase
      *
      * @return void
      */
-    public function testExample()
+    public function testIndexAsUser()
+    {
+        Passport::actingAs(factory(User::class)->create());
+
+        $response = $this->get('/api/calendar');
+
+        $response->assertStatus(200);
+    }
+
+    public function testIndexAsNoUser()
+    {
+        $response = $this->get('/api/calendar');
+
+        $response->assertStatus(302);
+        $response->assertRedirect('/login');
+    }
+
+    public function testStoreAsUser()
+    {
+        Passport::actingAs(factory(User::class)->create());
+
+        $event = factory(Calendar::class)->create();
+        $response = $this->json('POST', '/api/calendar', $event->toArray());
+
+        $response->assertStatus(200);
+    }
+
+    public function testStoreAsNoUser()
+    {
+        $response = $this->post('/api/calendar');
+
+        $response->assertStatus(302);
+        $response->assertRedirect('/login');
+
+    }
+
+    public function testUpdateAsUser()
+    {
+        Passport::actingAs(factory(User::class)->create());
+
+        $event = factory(Calendar::class)->create();
+
+        $data = [
+            'event_name' => 'Test',
+            'start_date'    => '2020-04-01',
+            'end_date'      => '2020-04-01',
+            'patient_email' => 'test@email.net'
+        ];
+
+        $this->put("api/calendar/$event->id", $data)
+             ->assertStatus(200);
+            
+    }
+
+    public function testUpdateAsNoUser()
+    {
+        $event = factory(Calendar::class)->create();
+
+        $data = [
+            'event_name' => 'Test',
+            'start_date'    => '2020-04-01',
+            'end_date'      => '2020-04-01',
+            'patient_email' => 'test@email.net'
+        ];
+
+        $this->put("api/calendar/$event->id", $data)
+             ->assertStatus(302)
+             ->assertRedirect('/login');
+
+    }
+
+    public function testDestroyAsUser()
+    {
+        Passport::actingAs(factory(User::class)->create());
+
+        $event = factory(Calendar::class)->create();
+
+        $this->delete("api/calendar/$event->id")
+             ->assertStatus(204);
+
+    }
+
+    public function testDestroyAsNoUser()
     {
         $response = $this->get('/');
 
-        $response->assertStatus(200);
+        $response->assertStatus(302);
+        $response->assertRedirect('/login');
+
     }
 }
