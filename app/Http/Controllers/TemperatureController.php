668d6217900e9038a57b0e62175d2b8fee1dbe03
cmhayden <haydencallum4@gmail.com>
2020-04-03 16:10:33 +0100

    create migration and resources for temperature feature

---
+++ app/Http/Controllers/TemperatureController.php
@@ -0,0 +1,85 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\temperature;
+use Illuminate\Http\Request;
+
+class TemperatureController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function index()
+    {
+        //
+    }
+
+    /**
+     * Show the form for creating a new resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function create()
+    {
+        //
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @return \Illuminate\Http\Response
+     */
+    public function store(Request $request)
+    {
+        //
+    }
+
+    /**
+     * Display the specified resource.
+     *
+     * @param  \App\temperature  $temperature
+     * @return \Illuminate\Http\Response
+     */
+    public function show(temperature $temperature)
+    {
+        //
+    }
+
+    /**
+     * Show the form for editing the specified resource.
+     *
+     * @param  \App\temperature  $temperature
+     * @return \Illuminate\Http\Response
+     */
+    public function edit(temperature $temperature)
+    {
+        //
+    }
+
+    /**
+     * Update the specified resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @param  \App\temperature  $temperature
+     * @return \Illuminate\Http\Response
+     */
+    public function update(Request $request, temperature $temperature)
+    {
+        //
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     *
+     * @param  \App\temperature  $temperature
+     * @return \Illuminate\Http\Response
+     */
+    public function destroy(temperature $temperature)
+    {
+        //
+    }
+}
