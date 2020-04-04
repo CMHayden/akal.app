984eb3d625e5d1e188ab5338461c70dd3d511429
cmhayden <haydencallum4@gmail.com>
2020-04-05 00:13:34 +0100

    create a new feature for sending emails

--- app/Http/Controllers/NotificationController.php
+++ app/Http/Controllers/NotificationController.php
@@ -4,6 +4,8 @@ namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
 use Twilio\Rest\Client;
+use Illuminate\Support\Facades\Mail;
+
 
 class NotificationController extends Controller
 {
@@ -19,5 +21,23 @@ class NotificationController extends Controller
         $twilio_number = getenv("TWILIO_NUMBER");
         $client = new Client($account_sid, $auth_token);
         $client->messages->create($recipients, ['from' => $twilio_number, 'body' => $message]);
+    }
+
+    public function sendEmail()
+    {
+        try
+        {        
+            $data = array('name'=>'John Doe', 'email'=>'johnDoe@gmail.com'); // Get user details
+
+            Mail::send('emails.welcome', $data, function($message) use ($data)
+            {
+                $message->to($data['email'], $data['name'])
+                        ->subject('Welcome');
+            });
+        }
+        catch (Exception $e)
+        {
+            throw new Exception($e);
+        }
     }
 }
