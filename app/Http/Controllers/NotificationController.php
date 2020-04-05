c2df2964673124fa63f1fcf2b578dd0920c75bf6
cmhayden <haydencallum4@gmail.com>
2020-04-05 15:45:29 +0100

    create logic and workflow for sending an email and sms to a user in regards to their patients home's temperature

--- app/Http/Controllers/NotificationController.php
+++ app/Http/Controllers/NotificationController.php
@@ -5,13 +5,82 @@ namespace App\Http\Controllers;
 use Illuminate\Http\Request;
 use Twilio\Rest\Client;
 use Illuminate\Support\Facades\Mail;
+use Symfony\Component\HttpFoundation\Response;
+use Illuminate\Support\Facades\Auth;
+use App\User; 
+use App\temperature;
+use App\HTTP\Resources\TemperatureResource;
+use App\HTTP\Resources\UserResource;
+
 
 
 class NotificationController extends Controller
 {
-    public function sendSMS()
+    public function alertTemperature($temp)
     {
-        $this->sendTextMessage("Message", "+000000000000"); // Get number for notification
+        if (Auth::check())
+        {
+            $userType = Auth::user()->userType;
+
+            if($userType == "carer")
+            {
+                return response()->json([
+                    'message' => 'This route is only for patient users.',
+                    'status' => 403
+                ]);
+            }
+            
+            else if($userType =="patient")
+            {
+                $patientEmail = auth::user()->email;
+
+                $users = UserResource::collection(User::where('patientEmail',"$patientEmail")->get());
+                
+                $temperatures = TemperatureResource::collection(Temperature::where('patientEmail',"$patientEmail")->get());
+
+                if($temperatures[0]->minTemp > $temp){
+                    $message = "Hey! This is Akal here to tell you your patient's home is too cold 🥶! This could lead to pneumonia or hypothermia if not rectified soon.";
+                    $subject = "Patient is too cold 🥶";
+
+                    foreach ($users as $user)
+                    {
+                        $data = ['name' => $user->name, 'email' => $user->email, 'subject' => $subject];
+
+                        $this->sendTextMessage($message, $user->phoneNumber);
+                        $this->sendEmail('emails.coldAlert', $data);
+                    }
+                    
+                } elseif($temperatures[0]->maxTemp < $temp){
+                    
+                    $message = "Hey! This is Akal here to tell you your patient's home is too hot 🥵! This could lead to dehydration if not rectified soon.";
+                    $subject = "Patient is too hot 🥵";
+
+                    foreach ($users as $user)
+                    {
+                        $data = ['name' => $user->name, 'email' => $user->email, 'subject' => $subject];
+
+                        $this->sendTextMessage($message, $user->phoneNumber);
+                        $this->sendEmail('emails.hotAlert', $data);
+                    }
+
+                } else {
+                    return response()->json([
+                        'message' => 'Good Temperature 😀',
+                        'status'=> 200
+                    ]);
+                }
+
+                return response()->json([
+                    'message' => 'Alerts sent correctly!',
+                    'status'=> 200
+                ]);
+            }
+        }    
+
+        return response()->json([
+            'message' => 'Check sentry ASAP',
+            'status' => 500
+        ]);
     }
 
     private function sendTextMessage($message, $recipients)
@@ -23,16 +92,14 @@ class NotificationController extends Controller
         $client->messages->create($recipients, ['from' => $twilio_number, 'body' => $message]);
     }
 
-    public function sendEmail()
+    public function sendEmail($view, $data)
     {
         try
         {        
-            $data = array('name'=>'John Doe', 'email'=>'johnDoe@gmail.com'); // Get user details
-
-            Mail::send('emails.welcome', $data, function($message) use ($data)
+            Mail::send($view, $data, function($message) use ($data)
             {
                 $message->to($data['email'], $data['name'])
-                        ->subject('Welcome');
+                        ->subject($data['subject']);
             });
         }
         catch (Exception $e)
