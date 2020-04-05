0850cead537e4885766110b4288d68e560b221e6
cmhayden <haydencallum4@gmail.com>
2020-04-05 23:53:17 +0100

    update migration to include filename, patient email and an image name

--- database/migrations/2020_04_05_225011_create_images_table.php
+++ database/migrations/2020_04_05_225011_create_images_table.php
@@ -15,6 +15,9 @@ class CreateImagesTable extends Migration
     {
         Schema::create('images', function (Blueprint $table) {
             $table->bigIncrements('id');
+            $table->string('filename');
+            $table->string('patientEmail');
+            $table->string('imageName');
             $table->timestamps();
         });
     }
