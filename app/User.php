c61ba2f6bdc2f52d345d6440c87ccaed5a98a005
cmhayden <haydencallum4@gmail.com>
2020-04-05 15:44:45 +0100

    add the fillable option of phone number for users

--- app/User.php
+++ app/User.php
@@ -16,7 +16,7 @@ class User extends Authenticatable
      * @var array
      */
     protected $fillable = [
-        'name', 'email', 'password', 'userType', 'patientEmail'
+        'name', 'email', 'password', 'userType', 'patientEmail', 'phoneNumber'
     ];
 
     /**
