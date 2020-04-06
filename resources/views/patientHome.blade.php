69babad7bdda61e4707df8d18099bd3745bd0e94
cmhayden <haydencallum4@gmail.com>
2020-04-06 13:33:25 +0100

    add image vie component to the patient view

--- resources/views/patientHome.blade.php
+++ resources/views/patientHome.blade.php
@@ -5,6 +5,8 @@
     <div class="row justify-content-center">
         <div class="col-md-4">
             <weather-component></weather-component>
+            <br>
+            <image-component></image-component>
         </div>
         <div class="col-md-8">
             <patient-calendar-component></patient-calendar-component>
