a49ceb645e21db714199652a9baecbe8244481c0
cmhayden <haydencallum4@gmail.com>
2020-04-08 13:19:50 +0100

    change the user factory to use a real patient's email

--- database/factories/UserFactory.php
+++ database/factories/UserFactory.php
@@ -24,7 +24,7 @@ $factory->define(User::class, function (Faker $faker) {
         'password' => '$2y$10$92IXUNpkjO0rOQ5byMi.Ye4oKoEa3Ro9llC/.og/at2.uheWG/igi', // password
         'remember_token' => Str::random(10),
         'userType' => 'carer',
-        'patientEmail' => $faker->safeEmail,
+        'patientEmail' => 'patient@akal.app',
         'phoneNumber' => env('TEST_NUMBER')
     ];
 });
